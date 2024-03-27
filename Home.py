69b3e17a8a9d676c37422d8c3301df04affacd9f
Twinkle Pardeshi <65865151+Twinkle110@users.noreply.github.com>
2024-03-27 15:55:03 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -6,12 +6,12 @@ def app():
       #  page_icon="🤖",
     #)
 
-    st.header("Talent Analytics Chatbot")
+    st.header("People Success Chatbot")
     st.sidebar.success("Select a page above.")
 
     # Add "Talent Analytics Chatbot" as content
     #st.header("Talent Analytics Chatbot")
-    st.write("""Welcome to the Talent Analytics Chatbot. I am an intelligent chatbot created by combining the strengths of Langchain and Streamlit. I use large language models to provide context-sensitive interactions. My goal is to help you better understand your data.
+    st.write("""Welcome to the People Success Chatbot. I am an intelligent chatbot created by combining the strengths of Langchain and Streamlit. I use large language models to provide context-sensitive interactions. My goal is to help you better understand your data.
       I support Image, PDF and Text prompt transcript 🧠""")
     
      
